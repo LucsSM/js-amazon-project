6ebea99618f15cdeb99cd65b949040d31e57a6d6
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-05-23 11:54:12 -0300

    refactor algorithms in amazon.js and moved addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,71 +1,81 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
 
 products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
-
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars * 10}.png">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+    productsHTML += `
+        <div class="product-container">
+        <div class="product-image-container">
+            <img class="product-image"
+            src="${product.image}">
         </div>
-      </div>
 
-      <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
-      </div>
+        <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+        </div>
 
-      <div class="product-quantity-container">
-        <select class="js-quantity-selector-${product.id}">
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
+        <div class="product-rating-container">
+            <img class="product-rating-stars"
+            src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+            ${product.rating.count}
+            </div>
+        </div>
 
-      <div class="product-spacer"></div>
+        <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+        </div>
 
-      <div class="added-to-cart js-added-message-${product.id}">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
+        <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            </select>
+        </div>
 
-      <button class="add-to-cart-button button-primary js-add-to-cart js-added-message"
-      data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>
-  `;
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart js-added-message-${product.id}">
+            <img src="images/icons/checkmark.png">
+            Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart js-added-message"
+        data-product-id="${product.id}">
+            Add to Cart
+        </button>
+        </div>
+    `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+    let cartQuantity = 0;
+
+        cart.forEach((cartItem) => {
+            cartQuantity += cartItem.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+}
 
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
   // Create and id to use in closure
-  let isMessageShownId;
-  
-  button.addEventListener('click', () => {
+    let isMessageShownId;
+    
+    button.addEventListener('click', () => {
     const { productId } = button.dataset;
     const productSelector = document.querySelector(`.js-quantity-selector-${productId}`);
     const quantity = Number(productSelector.value);
@@ -74,7 +84,7 @@ document.querySelectorAll('.js-add-to-cart')
 
     // check if the closure variable has the timeout id in it
     if(isMessageShownId) {
-      clearTimeout(isMessageShownId);
+        clearTimeout(isMessageShownId);
     };
 
     // run the timeout in a variable 
@@ -83,30 +93,7 @@ document.querySelectorAll('.js-add-to-cart')
     // save the timeout in the closure variable. The next time the button is pressed, it will contain the timeout and clear it in the if clause check. 
     isMessageShownId = showMessageId;
 
-    let matchingItem;
-    
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
+    addToCart(productId, quantity);
+    updateCartQuantity();
     });
-    
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId,
-        quantity
-      });
-    }
-    
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
-    });
-  });
+});
