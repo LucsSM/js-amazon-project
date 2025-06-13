1e21367d046806e7da472f3c5c9bd496695b1277
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-06-13 19:52:00 -0300

    added test for updateDeliveryOptions

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,6 +1,6 @@
-import {cart, addToCart, loadFromStorage, deleteProduct} from "../../data/cart.js";
+import {cart, addToCart, loadFromStorage, deleteProduct, updateDeliveryOption} from "../../data/cart.js";
 
-describe('TEST SUITE - addToCart', () => {
+describe('TEST SUITE - Cart functions', () => {
     beforeEach(() => {
         spyOn(localStorage, 'setItem');
     });
@@ -83,5 +83,44 @@ describe('TEST SUITE - addToCart', () => {
         expect(cart[0].quantity).toEqual(1);
     });
 
-    
+    it('updates the delivery option', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1,
+                deliveryOptionId: '1'
+            }]);
+        });
+        loadFromStorage();
+        updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', '3');
+        
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 1,
+            deliveryOptionId: '3'
+        }]));
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');    
+        expect(cart[0].deliveryOptionId).toEqual('3');
+    });
+
+    it('does nothing if a product is not in the cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1,
+                deliveryOptionId: '1'
+            }]);
+        });
+        loadFromStorage();
+        updateDeliveryOption('15b6fc6f-327a-4ec4-896f-486349e85a3d', '3');
+        
+        expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');    
+        expect(cart[0].quantity).toEqual(1);
+        expect(cart[0].deliveryOptionId).toEqual('1');
+    });
+
 })
