82894127053a218582683b8780ac812155470233
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-06-14 18:48:12 -0300

    created test for invalid delivery options

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -123,4 +123,22 @@ describe('TEST SUITE - Cart functions', () => {
         expect(cart[0].deliveryOptionId).toEqual('1');
     });
 
+    it('does nothing if the delivery option does not exist', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1,
+                deliveryOptionId: '1'
+            }]);
+        });
+        loadFromStorage();
+        updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', '0');
+        
+        expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');    
+        expect(cart[0].quantity).toEqual(1);
+        expect(cart[0].deliveryOptionId).toEqual('1');
+    });
+
 })
