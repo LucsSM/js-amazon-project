58dca15311c78e1aa7f519bb5c43da5c2d79836e
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-06-26 20:33:55 -0300

    created oop version of the cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderCartSummary} from "./chekout/cartSummary.js"
 import {renderOrderSummary} from "./chekout/orderSummary.js"
+import "../data/cart-opp.js"
 
 renderCartSummary();
 renderOrderSummary();
