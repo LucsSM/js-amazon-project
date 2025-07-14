b80a7331ac4d58ef8ed47d79466e1c4739e70d88
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-07-14 20:11:49 -0300

    intro to backend and XMLHttpRequest

---
+++ data/backend-practice.js
@@ -0,0 +1,8 @@
+const xml = new XMLHttpRequest();
+
+xml.addEventListener('load', () => {
+    console.log(xml.response);
+});
+
+xml.open('GET', 'https://supersimplebackend.dev');
+xml.send();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
-import {renderCartSummary} from "./chekout/cartSummary.js"
-import {renderOrderSummary} from "./chekout/orderSummary.js"
-import "../data/cart-class.js"
-import "../data/car.js"
+import {renderCartSummary} from "./chekout/cartSummary.js";
+import {renderOrderSummary} from "./chekout/orderSummary.js";
+import "../data/cart-class.js";
+import "../data/car.js";
+import "../data/backend-practice.js";
 
 renderCartSummary();
 renderOrderSummary();
