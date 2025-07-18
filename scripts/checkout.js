bb67fc826064daedf0c0a61b0b5bf563d10f5ac7
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-07-18 19:37:56 -0300

    used promises to load the producs, the cart and render the checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,10 +2,22 @@ import {renderCartSummary} from "./chekout/cartSummary.js";
 import {renderOrderSummary} from "./chekout/orderSummary.js";
 import "../data/cart-class.js";
 import {loadProducts} from "../data/products.js";
+import {loadCart} from "../data/cart.js";
 // import "../data/car.js";
 // import "../data/backend-practice.js";
 
-loadProducts(() =>{
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then(() => {
     renderCartSummary();
     renderOrderSummary();
-})
+});
