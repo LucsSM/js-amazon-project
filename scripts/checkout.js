a2bd452cd58b433885ca2fab9485bdbacb18af96
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-07-18 20:41:01 -0300

    new fuction to load the cart and use await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,22 +2,17 @@ import {renderCartSummary} from "./chekout/cartSummary.js";
 import {renderOrderSummary} from "./chekout/orderSummary.js";
 import "../data/cart-class.js";
 import {loadProductsFetch} from "../data/products.js";
-import {loadCart} from "../data/cart.js";
+import {loadCartFetch} from "../data/cart.js";
 // import "../data/car.js";
 // import "../data/backend-practice.js";
 
 async function loadPage() {
     await loadProductsFetch();
     
-    const value = await new Promise((resolve) => {
-        loadCart(() => {
-            resolve('resolve value test');
-        });
-    });
+    await loadCartFetch();
 
     renderCartSummary();
     renderOrderSummary();
-    console.log(value);
 };
 
 loadPage();
