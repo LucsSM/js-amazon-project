fa4acef6d12ee2430a037b7b83e926ac1e7fcda8
Lucas Silva Marra <lucas.silvamarra@gmail.com>
2025-07-18 20:05:39 -0300

    used fetch to load the products through the backend

--- data/products.js
+++ data/products.js
@@ -101,6 +101,29 @@ export function loadProducts(fun) {
 
 };
 
+export function loadProductsFetch() {
+    const promise = fetch('https://supersimplebackend.dev/products')
+    .then((response) => {
+        return response.json();
+    })
+    .then((productsData) => {
+        products = productsData.map((productDetails) => {
+            if(productDetails.type === "clothing") {
+                return new Clothing(productDetails);    
+            }
+
+            if(productDetails.type === "appliance") {
+                return new Appliance(productDetails);    
+            }
+            return new Product(productDetails);
+        });
+        
+        console.log('load products');
+    });
+
+    return promise;
+};
+
 /*
 export const products = [
     {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,13 @@
 import {renderCartSummary} from "./chekout/cartSummary.js";
 import {renderOrderSummary} from "./chekout/orderSummary.js";
 import "../data/cart-class.js";
-import {loadProducts} from "../data/products.js";
+import {loadProductsFetch} from "../data/products.js";
 import {loadCart} from "../data/cart.js";
 // import "../data/car.js";
 // import "../data/backend-practice.js";
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve();
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
